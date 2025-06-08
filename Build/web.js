afac6f166633566afa23ae3d98bdbc76e74f4ca1
aditya mishra <106386519+aditya29mishra@users.noreply.github.com>
2025-06-08 12:47:22 +0000

    code for web.js

---
+++ Build/web.js
@@ -0,0 +1,17 @@
+// web.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
+    });
+
+    router.get('/about', (req, res) => {
+        res.render('about', { title: 'About Us' });
+        });
+
+        router.get('/contact', (req, res) => {
+            res.render('contact', { title: 'Contact' });
+            });
+
+            module.exports = router;
